4396c9191782edeaa679932e34cbd772b139f8a3
z5160408 <z5160408@cse.unsw.edu.au>
2017-09-23 21:09:36 +1000

    added error checking

--- week9/image/Image.c
+++ week9/image/Image.c
@@ -11,8 +11,8 @@ typedef struct _image {
 
 Image newImage (unsigned int width, unsigned int height) {
     Image img = calloc (1, sizeof (image));
-    if (img img == null) {
-        err(EXIT_FAILURE, "calloc, failed");
+    if (img == null) {
+        errx(EXIT_FAILURE, "calloc, failed");
     }
 
 
@@ -22,10 +22,20 @@ Image newImage (unsigned int width, unsigned int height) {
     pixel **new;
 
     new = calloc (height, sizeof (pixel *));
+    
+    if (new == null) {
+        errx(EXIT_FAILURE, "calloc failed");
+    }
+    
     int i = 0;
 
     while (i < height) {
         new[i] = calloc (width, sizeof (pixel));
+        
+        if (new[i] == null) {
+            errx(EXIT_FAILURE, "calloc failed")'
+        }
+        
         i++;
     }
 
@@ -52,54 +62,4 @@ pixel imageGetPixel (Image i, point p) {
 
 void imageSetPixel (Image i, point p, pixel color) {
     i->data[p.y][p.x] = color;
-}
-
-static void print (Image img) {
-    int i = 0;
-    int j = 0;
-    
-    pixel col = {
-        .red = 0,
-        .green = 0,
-        .blue = 255
-    };
-    
-    while (i < img->height) {
-        while (j < img->width) {
-            point p = {
-                .x = j,
-                .y = i
-            };
-            
-            imageSetPixel(img, p, col);         
-
-            //pixel pix = imageGetPixel(img, p);
-
-            //printf("x: %d, y: %d\n", j, i); 
-            //printf("red: %d, green: %d, blue: %d \n", pix.red, pix.green, pix.blue); 
-
-            j++;
-        }
-
-        j = 0;
-        i++;
-    }
-}
-
-int main (int argc, char *argv[]) {
-    Image img = newImage(1000, 1000);
-    
-    print (img);
-    
-    unsigned char *buf;
-    int size = imageAsBMP (img, &buf);
-    fwrite (buf, sizeof (unsigned char), size, stdout);
-
-    free(buf);
-
-    destroyImage(img);
-
-    printf("Happy days\n");
-
-    return EXIT_SUCCESS;
 }
--- week9/image/testImage.c
+++ week9/image/testImage.c
@@ -20,7 +20,7 @@ int main (int argc, char *argv[]) {
     //Check
 
     //Check
-
+    generateImage(img);
     
     //Obeying newton
     destroyImage(i);
@@ -29,3 +29,47 @@ int main (int argc, char *argv[]) {
 
     return EXIT_SUCCESS;
 }
+
+
+static void genereateImage(Image i) {
+    
+    unsigned char *buf;
+    int size = imageAsBMP (i, &buf);
+    fwrite (buf, sizeof (unsigned char), size, stdout);
+
+    free(buf);
+
+}
+
+static void print (Image img) {
+    int i = 0;
+    int j = 0;
+    
+    pixel col = {
+        .red = 0,
+        .green = 0,
+        .blue = 255
+    };
+    
+    while (i < img->height) {
+        while (j < img->width) {
+            point p = {
+                .x = j,
+                .y = i
+            };
+            
+            imageSetPixel(img, p, col);         
+
+            //pixel pix = imageGetPixel(img, p);
+
+            //printf("x: %d, y: %d\n", j, i); 
+            //printf("red: %d, green: %d, blue: %d \n", pix.red, pix.green, pix.blue); 
+
+            j++;
+        }
+
+        j = 0;
+        i++;
+    }
+}
+
